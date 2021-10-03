9a5f7e6b3555c506686ce7ab07cea365015c9bde
你的name <>
2021-10-03 23:08:12 +0800

    chb

--- KPM.py
+++ KPM.py
@@ -1,24 +1,49 @@
 #建立NEXT数组
-
+import numpy as np
 def buildNext(x:str):
-    next=[]
-    next.append(0)
-    for i in x[1:]:
-        if (next[i-1] == 0) and x[i] == x[1]:
-            next.append(1)
-        elif next[i-1] == 0 and x[i] != x[1]:
-            next.append(0)
+    next=np.zeros(len(x))
+    next[0]=0
+    #print(type(next[1]))
+    i=1
+    while(i<len(x)):
+        if (next[i-1] == 0) and x[i] == x[0]:
+            next[i] = 1
+        elif next[i-1] == 0 and x[i] != x[0]:
+            next[i] = 0
         elif next [i-1] !=0:
-            if x[i] == x[next[i-1]]:
-                next[i].append(next[i-1]+1)
+            if x[i] == x[int(next[i-1])]:
+                next[i] = next[i-1]+1
             else:
-                if x[i] == x[1]:
-                    next.append(1)
+                if x[i] == x[0]:
+                    next[i] = 1
                 else:
-                    next.append(0)
+                    next[i] = 0
+        i+=1
     return next
 
+def search(sstr:str, x:str):#x是子串
+    res=[]#用于存放答案
+    i=0#原串指针
+    j=0#子串指针
+    next_str = buildNext(sstr)
+    while(i<len(sstr)):
+        while(j<len(x)):
+            if sstr[int(i+j)] == x[int(j)]:
+                j+=1
+            else:
+                #不等的话就跳转
+                i = i+(j-1)-(next_str[j]-1)
+                j=0
+                break
+        if(j==len(x)):
+            res.append(i)
+            j=0
+        i+=1
+    return res
 
-aa = "abcabd"
-kk = buildNext(aa)
-print(kk)
+strr = "imapigpig"
+aa = search(strr, "pig")
+print(aa)
+
+
+
