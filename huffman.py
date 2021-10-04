95c1ed30503ddfa4ba460547c8a0f5c12914a054
chb <>
2021-10-04 23:03:21 +0800

    tmd

--- huffman.py
+++ huffman.py
@@ -1,5 +1,6 @@
 #先统计词频用hashmap
 import collections as cl
+
 def freq(q:str) ->dict:
     di = cl.Counter(q)
     return di
@@ -26,10 +27,39 @@ class huffman:
             self.Huffbuf.append(temp)
         self.Root = self.Huffbuf[0]#用于存放根节点
         self.codes = range(1)#用于存放编码
-        print(self.Huffbuf[0].value)
+        self.stack = []
+       # self.flag = False
+        #print(self.Huffbuf[0].value)
 
 
-    def encoding(self):
+    def encoding(self, root:node, flag:bool):
+        #左零右一tlr
+        if root != None:
+            if root.name != '@':
+                print(root.name+':'+aa(self.stack))
+            elif flag==False:
+                self.stack.append(0)
+                print(self.stack)
+            elif flag == True:
+                self.stack.pop(-1)
+                self.stack.append(1)
+                print(self.stack)
+            self.encoding(root.left, False)
+            #证明左边数完了改到右边了
+            self.stack.pop(-1)
+            self.stack.append(1)
+            print(self.stack)
+            print("ww")
+            self.encoding(root.right, True)
+            #能到这里说明向右走结束了
 
+
+def aa(nums:list) ->str:
+    m = ''
+    for i in range(len(nums)):
+        x = str(nums[i])
+        m+=x
+    return m
 a = "abcd"
-huffman(a)
+k = huffman(a)
+k.encoding(k.Root,False)
