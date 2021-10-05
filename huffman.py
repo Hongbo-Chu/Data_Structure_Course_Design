acf60bb04d9b1d36e588c45f16a8f1dcbfcd5b14
chb <>
2021-10-05 10:45:22 +0800

    sb

--- huffman.py
+++ huffman.py
@@ -26,33 +26,78 @@ class huffman:
             temp.right = self.Huffbuf.pop(-1)
             self.Huffbuf.append(temp)
         self.Root = self.Huffbuf[0]#用于存放根节点
-        self.codes = range(1)#用于存放编码
-        self.stack = []
+        #self.codes = range(1)#用于存放编码
+        self.codes = []
+        self.b = [0]*10
        # self.flag = False
         #print(self.Huffbuf[0].value)
 
 
-    def encoding(self, root:node, flag:bool):
-        #左零右一tlr
-        if root != None:
-            if root.name != '@':
-                print(root.name+':'+aa(self.stack))
-            elif flag==False:
-                self.stack.append(0)
-                print(self.stack)
-            elif flag == True:
-                self.stack.pop(-1)
-                self.stack.append(1)
-                print(self.stack)
-            self.encoding(root.left, False)
-            #证明左边数完了改到右边了
-            self.stack.pop(-1)
-            self.stack.append(1)
-            print(self.stack)
-            print("ww")
-            self.encoding(root.right, True)
-            #能到这里说明向右走结束了
+    # def encoding(self, root:node, flag:bool):
+    #     #左零右一tlr
+    #     if root != None:
+    #         if root.name != '@':
+    #             print(root.name+':'+aa(self.stack))
+    #         elif flag==False:
+    #             self.stack.append(0)
+    #             print(self.stack)
+    #         elif flag == True:
+    #             self.stack.pop(-1)
+    #             self.stack.pop(-1)
+    #             self.stack.append(1)
+    #             print(self.stack)
+    #         self.encoding(root.left, False)
+    #         #证明左边数完了改到右边了
+    #         self.stack.pop(-1)
+    #         self.stack.append(1)
+    #         print(self.stack)
+    #         print("ww")
+    #         self.encoding(root.right, True)
+    #         #能到这里说明向右走结束了
 
+    #非递归前序遍历实现
+    # def encoding(self):
+    #     tt = self.Root
+    #     stack = []
+    #     code_stack = []
+    #     while tt!=None or len(stack) != 0:
+    #         if tt!=None:
+    #             code_stack.append(list(self.codes))
+    #             if tt.left!=None:
+    #                 stack.append(tt)
+    #                 self.codes.append(0)
+    #
+    #             if tt.name!='@':
+    #                 print(tt.name+':'+aa(self.codes))
+    #             tt=tt.left
+    #         else:
+    #             tt = stack[len(stack)-1]
+    #             stack.pop(-1)
+    #             self.codes = code_stack[len(code_stack)-1]
+    #             code_stack.pop(-1)
+    #             self.codes.pop(-1)
+    #             self.codes.append(1)
+    #             tt=tt.right
+    #             # self.codes.append(1)
+
+
+    def pre(self,tree,length):
+        node=tree
+        if (not node):
+            return
+        elif node.name!='@':
+            print (node.name + '的编码为:')
+            for i in range(length):
+                print (self.b[i])
+            print( '\n')
+            return
+        self.b[length]=0
+        self.pre(node.left,length+1)
+        self.b[length]=1
+        self.pre(node.right,length+1)
+     #生成哈夫曼编码
+    def get_code(self):
+        self.pre(self.Root,0)
 
 def aa(nums:list) ->str:
     m = ''
@@ -62,4 +107,4 @@ def aa(nums:list) ->str:
     return m
 a = "abcd"
 k = huffman(a)
-k.encoding(k.Root,False)
+k.get_code()
