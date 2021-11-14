f8a6e48b91c4c4876775dad65d30b832909cc86f
chb <>
2021-11-14 15:59:26 +0800

    实现了网页端的搜索

--- test/index.py
+++ test/index.py
@@ -2,24 +2,36 @@ from flask import Flask, render_template,jsonify,request
 import sys
 sys.path.append('../back/')
 from huffman_final import huffman
+from search import searcher
 app = Flask(__name__)
 
 @app.route('/homepage')
 def hello():
     # return "212"
     return render_template('homepage.html')
+# #显示中文文本
+# @app.route('/jsondemo', methods = ['post'])
+# def jsondemo():
+#     print('haha')
+#     info = request.json.get('name')
+#     print(info)
 
+#     path = "D:/vscodePythonSpace/Data_Structure_Course_Design/cs_data/"
+#     path += str(info)
+#     with open(path, "r") as f:
+#         data = f.read()
+#     return data
+
+#显示搜索结果
 @app.route('/jsondemo', methods = ['post'])
 def jsondemo():
     print('haha')
     info = request.json.get('name')
     print(info)
+    ans = searcher(info)
+    return ans
 
-    path = "D:/vscodePythonSpace/Data_Structure_Course_Design/cs_data/"
-    path += str(info)
-    with open(path, "r") as f:
-        data = f.read()
-    return data
+
 
 @app.route('/encode', methods = ['post'])
 def encode():
